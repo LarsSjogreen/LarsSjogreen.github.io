12703793e3a621d9de7750188f175c5f397386e0
Lars Sjögreen <lars.sjogreen@akvo.org>
2018-05-01 21:54:42 +0200

    Fuskar med att lägga grejor i global namnrymd

--- serviceworker.js
+++ serviceworker.js
@@ -17,7 +17,7 @@ self.addEventListener('install', function(event) {
     caches.open(CACHE_NAME)
       .then(function(cache) {
         console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+        return cache.addAll(window.urlsToCache);
       })
   );
 });
